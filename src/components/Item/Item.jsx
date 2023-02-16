92fe736eaa4c0af98ca0a7e832ae68f896888e35
John Mayou <johnm.ceo@gmail.com>
2023-02-16 14:49:29 -0600

    fix react errors

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,40 +1,42 @@
 import "./Item.css";
-import axios from 'axios';
+import axios from "axios";
 import { useState } from "react";
+
 function Item({ getItems, item }) {
-
-
 	const deleteItem = (e) => {
 		const id = e.target.value;
 		console.log(id);
-		axios.delete(`/item/${id}`)
-				.then(() => {
-					getItems(); 
-				})
-				.catch((err) => {
-					console.log('Delete request error', err)
-				})
-			}
-
-			const updateItem = (id =>{
-				console.log('click updateItem',id)
-				axios.put(`/items/${id}`)
-				.then(response => {
-					console.log('in app updateItem axios.then')
-					getItems();
-			
-				})
-				.catch(err => {
-					alert('error updating items in app axios.put.catch')
-				})
-				
+		axios
+			.delete(`/item/${id}`)
+			.then(() => {
+				getItems();
+			})
+			.catch((err) => {
+				console.log("Delete request error", err);
 			});
+	};
+
+	const updateItem = (id) => {
+		console.log("click updateItem", id);
+		axios
+			.put(`/items/${id}`)
+			.then((response) => {
+				console.log("in app updateItem axios.then");
+				getItems();
+			})
+			.catch((err) => {
+				alert("error updating items in app axios.put.catch");
+			});
+	};
 
 	return (
-	<div>
-		<button value={item.id} onClick={(e) => deleteItem(e)}>Delete</button>
-		<button>UPDATE</button>
-	</div>);
+		<div>
+			<button value={item.id} onClick={(e) => deleteItem(e)}>
+				Delete
+			</button>
+			<button>UPDATE</button>
+		</div>
+	);
 }
 
 export default Item;
