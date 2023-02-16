99fc90d2b265d9a85ae1d0dc68c52de99e3332be
Andrea Love <andrealovelovelove@gmail.com>
2023-02-16 14:04:06 -0600

    added delete item on item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,7 +1,25 @@
 import "./Item.css";
+import axios from 'axios';
 
-function Item({ getItems, itemList }) {
-	return <div></div>;
+function Item({ getItems, item }) {
+
+
+	const deleteItem = (e) => {
+		const id = e.target.value;
+		console.log(id);
+		axios.delete(`/item/${id}`)
+				.then(() => {
+					getItems(); 
+				})
+				.catch((err) => {
+					console.log('Delete request error', err)
+				})
+			}
+
+	return (
+	<div>
+		<button value={item.id} onClick={(e) => deleteItem(e)}>Delete</button>
+	</div>);
 }
 
 export default Item;
