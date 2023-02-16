3a4706d4490f7738d732691df01f011f7164980a
Zakariye Abdi <zak.abdi94@gmail.com>
2023-02-16 14:15:09 -0600

    Completed both Put sides

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,7 +1,37 @@
 import "./Item.css";
+import axios from "axios";
+import { useState } from "react";
 
 function Item({ getItems, itemList }) {
-	return <div></div>;
-}
 
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	const updateItem = (id =>{
+		console.log('click updateItem',id)
+		axios.put(`/items/${id}`)
+		.then(response => {
+			console.log('in app updateItem axios.then')
+			getItems();
+	
+		})
+		.catch(err => {
+			alert('error updating items in app axios.put.catch')
+		})
+		
+	});
+		<button>UPDATE</button>
+	
+	return <div></div>;
+
+}
 export default Item;
